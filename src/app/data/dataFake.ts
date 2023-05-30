45fd7a47c6c3f7797b3c461805b180d05fe89f9d
Naurfps <ruanlucasafs37@gmail.com>
2023-05-30 17:31:10 -0300

    details on the content

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -9,7 +9,19 @@ export const dataFake = [
   {
     "id":"2",
     "title": "NOVA FASE ANUNCIADA DA MARVEL!",
-    "description": "Nova fase da Marvel e anunciada, confira.",
+    "description": "Nova fase da Marvel e anunciada durante a ComicCon",
     "photoCover": "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/7ec0067a-460f-11ec-aa6e-9587410378a2/marvel-disney-plus-shows.jpg"
+  },
+  {
+    "id":"3",
+    "title": "NOVO JOGO DO HOMEM-ARANHA EM DESENVOLVIMENTO!",
+    "description": "Nova jogo do Marvel Spiderman é anunciado, confira.",
+    "photoCover": "https://sm.ign.com/ign_br/screenshot/default/marvels-spider-man-2_qq7b.jpg"
+  },
+  {
+    "id":"4",
+    "title": "NOVO DRAGON BALL BUDOKAI TENKAICHI EM DESENVOLVIMENTO!",
+    "description": "Novo Trailer do novo jogo do Dragon Ball foi exibido no Torneio de Dragon Ball FighterZ.",
+    "photoCover": "https://sm.ign.com/ign_br/screenshot/default/dragon-ball-hour-battle-hour-2023-revela-dragon-ball-budokai_e74d.jpg"
   }
 ]
