fcb329f23f89a3115433e016eefcd57326fe1787
Naurfps <ruanlucasafs37@gmail.com>
2023-05-30 17:23:21 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVA VERSAO DO ANGULAR!",
+    "description": "Nova versao do Angular esta sendo feita pela empresa fundadora.",
+    "photoCover": "https://i.pinimg.com/originals/b5/fe/11/b5fe11ee521e206fca92bc865d89c742.png"
+  },
+  {
+    "id":"2",
+    "title": "NOVA FASE ANUNCIADA DA MARVEL!",
+    "description": "Nova fase da Marvel e anunciada, confira.",
+    "photoCover": "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/7ec0067a-460f-11ec-aa6e-9587410378a2/marvel-disney-plus-shows.jpg"
+  }
+]
